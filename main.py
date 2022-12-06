dd8109a0a2cdf8e5bebcdefa41e42954778c4543
SD6399 <david-shapiro@mail.ru>
2022-12-06 16:34:15 +0300

    add new structure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 from PIL import Image
 from keras.models import load_model
 from ANN import build_ANN
@@ -6,19 +6,39 @@ from ANN import val_2_tensor
 from ANN import apply
 from ANN import find
 from skimage import io
-path = 'D:/new_dataset/'
 
-# creating model
-# new_model=build_ANN(path, 128, 128, True, True, True)
-# new_model.save('gfgModel.h5')
+path = r"C:\Users\user\PycharmProjects\image_hamming\DATA"
 
-savedModel = load_model('gfgModel.h5')
 
-for i in range(200,600):
-    Y=apply(i,savedModel)
-    img = Image.fromarray(Y.astype('uint8'))
-    img.convert('RGB').save(r"D:\cut_image\image" + str(i) + ".png")
+def CreateModel(pathToImages, nameToSaveModel, size):
+    model = build_ANN(pathToImages, size, size, use_flip=False, use_rotate=False, extend_flag=False)
+    model.save(nameToSaveModel)
 
-for i in range(200,600):
-    img=io.imread(r"D:\cut_image\image"+str(i)+".png")
-    print(find(img,savedModel))
+
+def UseModel(modelName, fromRange, toRange, pathToSave):
+    savedModel = load_model(modelName)
+    for i in range(fromRange, toRange):
+        Y = apply(i, savedModel)
+        img = Image.fromarray(Y.astype('uint8'))
+        img.convert('RGB').save(pathToSave + "\img_" + str(i) + ".png")
+
+
+def ReverseModel(modelName, fromRange, toRange, imagesPath):
+    savedModel = load_model(modelName)
+    for i in range(fromRange, toRange):
+        img = io.imread(imagesPath + "\img_" + str(i) + ".png")
+        ind = find(img, savedModel)
+        print("i = " + str(i) + ";   IND = " + str(ind))
+
+
+if __name__ == "__main__":
+    modelName = path + "\gfgModel.h5"
+
+    # ------ создание
+    CreateModel(path + "/IN/", modelName, 512)
+
+    # ----- использование
+    UseModel(modelName, 1, 35, path + "\OUT")
+
+    # ------ реверс
+    # ReverseModel(modelName, 11, 15, path + "\OUT" )
